942ed933bd75b5ecbf3cc3cc989e7b37c739fab1
Andriy Zherdiy <andryuha49@windowslive.com>
2019-02-18 23:13:50 +0200

    Updated readme Added i18n

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # node-express-server
 Simple node server with express
+
+
+## Examples
+```typescript
+import {server} from 'node-express-server';
+import {UsersController} from './controllers/usersController';
+
+// Use i18n
+server.useI18n({directory: __dirname + '/assets/i18n'});
+
+// Use routing-controllers
+server.useApi([UsersController]);
+
+// Use swagger
+server.useSwagger('/api/swagger-ui');
+
+// Use static
+server.useStatic(__dirname + '/../clint');
+
+server.start(3001);
+```
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
 import express from 'express';
 import {useExpressServer} from 'routing-controllers';
 import {useSwagger} from './useSwagger';
+import {useI18n} from './useI18n';
 
 const server: any = express();
 
 const defaultSwaggerPatch = '/api/swagger-ui';
 const defaultSwaggerJsonPatch = '/api/v1/swagger.json';
-server.useSwagger = () => {
-  return useSwagger(server, defaultSwaggerPatch, defaultSwaggerJsonPatch);
-};
+// server.useSwagger = () => {
+//   return useSwagger(server, defaultSwaggerPatch, defaultSwaggerJsonPatch);
+// };
 
 server.useSwagger = (swaggerPatch: string = '/api/swagger-ui', swaggerJsonPatch: string = '/api/v1/swagger.json') => {
   swaggerPatch = swaggerPatch || defaultSwaggerPatch;
   swaggerJsonPatch = swaggerJsonPatch || defaultSwaggerJsonPatch;
   return useSwagger(server, swaggerPatch, swaggerJsonPatch);
+};
+
+server.useI18n = (i18nConfig: any = null) => {
+  return useI18n(server, i18nConfig);
+};
+
+server.useApi = (controllers: string[] | any[]) => {
+  useExpressServer(server, { // register created express server in routing-controllers
+    controllers: controllers // and configure it the way you need (controllers, validation, etc.)
+  });
+};
+
+server.useStatic = (route: string, contentPath: string) => {
+  server.use(route, express.static(contentPath));
 };
 
 /**
@@ -28,11 +43,7 @@ server.start = (port: number, nameOrOptions: string | any, options: any = {}) =>
     name = nameOrOptions;
   }
 
-  useExpressServer(server, { // register created express server in routing-controllers
-    controllers: server.controllers // and configure it the way you need (controllers, validation, etc.)
-  });
-
-  return server.listen(port, () => {
+  return server.listen(port, (callbackParams: any) => {
     console.log(
       '  App is running at http://localhost:%d in %s mode. Service: %s',
       port,
@@ -40,6 +51,9 @@ server.start = (port: number, nameOrOptions: string | any, options: any = {}) =>
       name
     );
     console.log('  Press CTRL-C to stop\n');
+    if (options.callback) {
+      options.callback(callbackParams);
+    }
   });
 };
 
